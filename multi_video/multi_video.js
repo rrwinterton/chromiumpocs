1b0800b02752f9c14679650acbf8691514b636f1
Richard Winterton <rrwinterton@gmail.com>
2023-03-03 17:29:21 -0700

    initial checkin

---
+++ multi_video/multi_video.js
@@ -0,0 +1,12 @@
+/* multi_video.js */
+
+//isElementInViewport determines window and documentElement rect
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
